1f558f8e82fc064eade429be481bb6c4da1d53ab
btlenny <btlenny@gmail.com>
2023-10-26 20:50:44 -0700

    complete i think

--- clues.sql
+++ clues.sql
@@ -44,6 +44,9 @@ WHERE countrycode = 'SMR' AND Name != 'San Marino';
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
+SELECT * FROM city
+WHERE Name LIKE 'Serra%' AND Countrycode != 'SMR';
+
 -- Write SQL query here
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
@@ -52,6 +55,9 @@ WHERE countrycode = 'SMR' AND Name != 'San Marino';
 
 -- Write SQL query here
 
+SELECT city.Name
+FROM city
+WHERE city.countrycode = 'BRA' AND city.id = 211;
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -68,4 +74,8 @@ WHERE countrycode = 'SMR' AND Name != 'San Marino';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT * FROM city
+WHERE POPULATION = 91084
+
 
